ae6b17045a945b717943be635aa79802cfaeb524
Jack Kim <jack@jkim-mbp.local>
2021-03-02 16:18:06 -0800

    Add basic table

---
+++ src/components/BasicTable.js
@@ -0,0 +1,76 @@
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './columns';
+import './table.css';
+
+const BasicTable = () => {
+
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  })
+
+  return (
+    // Apply the table props
+    <table {...getTableProps()}>
+      <thead>
+        {// Loop over the header rows
+          headerGroups.map(headerGroup => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              { // Loop over the headers in each row
+                headerGroup.headers.map( column => (
+                  // Apply the header cell props
+                  <th {...column.getHeaderProps()}>
+                    { // Render the header
+                      column.render('Header')
+                    }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {// Loop over the table rows
+          rows.map(row => {
+            // Prepare the row for display
+            prepareRow(row)
+            return (
+              // Apply the Row Props
+              <tr {...row.getRowProps()}>
+                {
+                  // Loop over the rows cells
+                  row.cells.map( cell => {
+                    // Apply the cell props
+                    return (
+                      <td {...cell.getCellProps()}>
+                        {// Render the cell contents
+                          cell.render('Cell')
+                        }
+                      </td>
+                    )
+                  })
+                }
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
+}
+
+export default BasicTable;
